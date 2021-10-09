feece73efd31be03e351ef4c9996c078c5a5898a
Tanathip64070045 <88467996+Tanathip64070045@users.noreply.github.com>
2021-10-09 22:20:44 +0700

    ttesxtttttttttt

--- escape game.py
+++ escape game.py
@@ -1,7 +1,14 @@
 """escape game"""
-print("hello word")
-print("hello")
-"""1111"""
-"""2222"""
-"""3333"""
-"""4444"""
+import pygame
+def control():
+    """ควบคุม"""
+    pygame.init()
+    screen = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("TEST")
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                quit()
+control()
+    
