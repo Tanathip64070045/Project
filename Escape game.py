9640403c3892c971cd2ef6474dfa3256be0bd7a6
Tanathip64070045 <88467996+Tanathip64070045@users.noreply.github.com>
2021-10-12 19:07:48 +0700

    add ตัวการ์ตูน

--- Escape game.py
+++ Escape game.py
@@ -14,7 +14,7 @@ class Soldier(pygame.sprite.Sprite):
     def __init__(self, x, y, scale):
         pygame.sprite.Sprite.__init__(self)
         img = pygame.image.load('0.png')
-        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.image = pygame.transform.scale(img, (int(100+scale), int(100+scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
