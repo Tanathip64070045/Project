0fd1879dd0af267541e08db899e4a3176d9fe4de
Tanathip64070045 <88467996+Tanathip64070045@users.noreply.github.com>
2021-10-12 18:48:43 +0700

    display

--- Escape game.py
+++ Escape game.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 
 
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,7 +13,7 @@ pygame.display.set_caption('Shooter')
 class Soldier(pygame.sprite.Sprite):
     def __init__(self, x, y, scale):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('img/player/Idle/0.png')
+        img = pygame.image.load('0.png')
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
