57ed5f4aa8c35b06b011e5e74685e39abda4c9f7
Tanathip64070045 <88467996+Tanathip64070045@users.noreply.github.com>
2021-10-14 22:23:39 +0700

    moving and press keys

--- Escape game.py
+++ Escape game.py
@@ -1,46 +1,39 @@
+"""Escap game"""
+
 import pygame
-
+######### game set display   ################
 pygame.init()
+screen = pygame.display.set_mode((1000,800))
+pygame.display.set_caption("ESCAPE GAME")
+icon = pygame.image.load("icon.png")
+pygame.display.set_icon(icon)
+player = pygame.image.load("0.png")
+player1 = pygame.transform.scale(player,(50,50))
+
+##################### ตำแหน่ง PLAYER #########################
+posX = 0
+posY = 800-50
+move = 5
+
+######################  RUN GAME #########################
+while True:
+	pygame.time.delay(12)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			quit() # ออก
 
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Shooter')
-
-
-class Soldier(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale):
-        pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('0.png')
-        self.image = pygame.transform.scale(img, (int(40), int(40)))
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-
-    def draw(self):
-        screen.blit(self.image, self.rect)
-
-
-
-player = Soldier(200, 200, 3)
-
-
-
-
-run = True
-while run:
-
-    
-    player.draw()
-    
-
-    for event in pygame.event.get():
-        #quit game
-        if event.type == pygame.QUIT:
-            run = False
-
-
-    pygame.display.update()
-
-pygame.quit()
+	#################### PRESS KEY ############################
+	keys = pygame.key.get_pressed()
+	if keys[pygame.K_a] and posX > 0: # LEFT
+		posX -= move
+	if keys[pygame.K_d] and posX < 1000 - 50: # RIGHT
+		posX += move
+	if keys[pygame.K_w] and posY > 0: # UP
+		posY -= move
+	if keys[pygame.K_s] and posY < 750: # DOWN
+		posY += move
+	
+	###################### แสดงผล ################################
+	screen.blit(player1,(posX, posY)) # แสดงผลตัวละคร
+	pygame.display.update()   # อัปเดรตหน้าจอ
