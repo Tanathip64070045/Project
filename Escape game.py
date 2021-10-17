39cf14c9f7aaaf289d29d1546f89024ef5fa9325
Tanathip64070045 <88467996+Tanathip64070045@users.noreply.github.com>
2021-10-17 20:54:42 +0700

    ทำตัวละคร ปรับsmooth

--- Escape game.py
+++ Escape game.py
@@ -1,41 +1,42 @@
 """Escap game"""
 
-import pygame
+import pygame 																				# import
 def main():
 ######### game set display   ################
-	pygame.init()
-	screen = pygame.display.set_mode((1000,800))
-	pygame.display.set_caption("ESCAPE GAME")
-	icon = pygame.image.load("icon.png")
-	pygame.display.set_icon(icon)
-	player = pygame.image.load("0.png")
-	player1 = pygame.transform.scale(player,(30,30))
-
+	pygame.init() 																			# create
+	screen = pygame.display.set_mode((1000,800)) 											# display
+	pygame.display.set_caption("ESCAPE GAME") 												# title game
+	icon = pygame.image.load("icon.png")  													# icon game
+	pygame.display.set_icon(icon)    														# set icon
+	player = pygame.image.load("0.png") 													# image ตัวละคร
+	player1 = pygame.transform.scale(player,(30,30)) 										# ขนาด player
 ##################### ตำแหน่ง PLAYER #########################
-	posX = 0
-	posY = 800-30
-	move = 5
+	posX = 0         																		#แกน x (ซ้าย- ขวา)
+	posY = 800-30   																		# แกน y  (ขึ้น - ลง)
+	move = 2		 																		# จำนวนย้าย
 
 ######################  RUN GAME #########################
 	while True:
-		pygame.time.delay(12)
-		for event in pygame.event.get():
+		pygame.time.delay(20) 																# delay 
+		BG = (0, 0, 0) 																		# BG สีดำ (RGB)
+		screen.fill(BG) 																	# ใส่ พื้นหลัง
+		for event in pygame.event.get(): 													# ทำอีเว้น
 			if event.type == pygame.QUIT:
-				quit() # ออก
+				quit()																		# ออก
 
 
 	#################### PRESS KEY ############################
-		keys = pygame.key.get_pressed()
-		if keys[pygame.K_a] and posX > 0: # LEFT
+		keys = pygame.key.get_pressed() 													# กำหนด keys เป็น การกด
+		if keys[pygame.K_a] and posX > 0: 													# LEFT
 			posX -= move
-		if keys[pygame.K_d] and posX < 1000 - 30: # RIGHT
+		if keys[pygame.K_d] and posX < 1000 - 30: 											# RIGHT
 			posX += move
-		if keys[pygame.K_w] and posY > 0: # UP
+		if keys[pygame.K_w] and posY > 0: 													# UP
 			posY -= move
-		if keys[pygame.K_s] and posY < 770: # DOWN
+		if keys[pygame.K_s] and posY < 770: 												# DOWN
 			posY += move
 	
 	###################### แสดงผล ################################
-		screen.blit(player1,(posX, posY)) # แสดงผลตัวละคร
-		pygame.display.update()   # อัปเดรตหน้าจอ
+		screen.blit(player1,(posX, posY)) 													# แสดงผลตัวละคร
+		pygame.display.update()   															# อัปเดรตหน้าจอ
 main()
