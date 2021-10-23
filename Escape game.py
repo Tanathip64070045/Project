1d1462c1e7c28f89eeef1471984f86db66d663fe
64070127_Ken muraki <88493438+kenmuraki@users.noreply.github.com>
2021-10-23 12:53:29 +0700

    Update Escape game.py

--- Escape game.py
+++ Escape game.py
@@ -6,8 +6,8 @@ screen = pygame.display.set_mode((1000,750))
 pygame.display.set_caption("ESCAPE GAME") 													
 icon = pygame.image.load("picture/icon.png")  														
 pygame.display.set_icon(icon)    															
-player = pygame.image.load("picture/char.png") 														
-player1 = pygame.transform.scale(player,(50,50)) 											
+player = pygame.image.load("picture/ufo.png") 														
+player1 = pygame.transform.scale(player,(40,40)) 											
 
 """ ตำแหน่งตัวละคร """
 posX = 0         																			
