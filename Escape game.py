72c8915c42dcabf5196113f79c2b8ae1f1b6d9fc
64070127_Ken muraki <88493438+kenmuraki@users.noreply.github.com>
2021-10-23 15:36:05 +0700

    Update Escape game.py

--- Escape game.py
+++ Escape game.py
@@ -6,8 +6,8 @@ screen = pygame.display.set_mode((1000,750))
 pygame.display.set_caption("ESCAPE GAME") 													
 icon = pygame.image.load("picture/icon.png")  														
 pygame.display.set_icon(icon)    															
-player = pygame.image.load("picture/ufo.png") 														
-player1 = pygame.transform.scale(player,(40,40)) 											
+player = pygame.image.load("picture/tjakchar.png") 														
+player1 = pygame.transform.scale(player,(80,80)) 											
 																																	
 #################### bg หลังการเดิน 1 ครั้ง ######################
 def down_bg():
@@ -56,7 +56,7 @@ def main():
 	"""RUN"""
     
 	posX = 0         																		
-	posY = 750-40   																		
+	posY = 750-80   																		
 	move = 2																				
 	""" loop รันเกม """
 	while True:
@@ -70,11 +70,11 @@ def main():
 		keys = pygame.key.get_pressed() 													
 		if keys[pygame.K_a] and posX > 0: 													
 			posX -= move
-		if keys[pygame.K_d] and posX < 1000 - 40: 											
+		if keys[pygame.K_d] and posX < 1000 - 80: 											
 			posX += move
 		if keys[pygame.K_w] and posY > 0: 													
 			posY -= move
-		if keys[pygame.K_s] and posY < 750 - 40: 											
+		if keys[pygame.K_s] and posY < 750 - 80: 											
 			posY += move
 		screen.blit(player1,(posX, posY)) 													
 		pygame.display.update()   															
